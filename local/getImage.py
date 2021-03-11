2cda447c47f8c81ed1d0322c39c1d0154bd88963
rakshitraj <rraj.rakshit@gmail.com>
2021-03-11 17:27:46 +0530

    modified getImages

--- local/getImage.py
+++ local/getImage.py
@@ -2,28 +2,29 @@ import cv2
 import time
 import os.path
 
-key = cv2.waitKey(1)
-camera = cv2.VideoCapture(0)
-while True:
-    try:
-        check, frame = camera.read()
-        
-        # # Encoding to JPEG
-        # success, buffer = cv2.imencode(".jpg", frame)
-        
-        ## System Checks
-        # print(check)
-        #
-        # print(frame) # matrix value of images
-        # cv2.imshow("Capturing", frame)
+def recordData() :
+    key = cv2.waitKey(1)
+    camera = cv2.VideoCapture(0)
+    while True:
+        try:
+            check, frame = camera.read()
+            
+            # # Encoding to JPEG
+            # success, buffer = cv2.imencode(".jpg", frame)
+            
+            ## System Checks
+            # print(check)
+            #
+            # print(frame) # matrix value of images
+            # cv2.imshow("Capturing", frame)
 
-        # Save images
-        now = time.strftime('%d-%m-%Y %H%M%S')
-        filename = now+'.jpg'
-        path = 'local/raw'
-        cv2.imwrite(filename=os.path.join(path, filename), img=frame)
-        camera.release()
-        break
-    except:
-        # Turn off camera
-        camera.release()
+            # Save images
+            now = time.strftime('%d-%m-%Y %H%M%S')
+            filename = now+'.jpg'
+            path = 'local/raw'
+            cv2.imwrite(filename=os.path.join(path, filename), img=frame)
+            camera.release()
+            break
+        except:
+            # Turn off camera
+            camera.release()
