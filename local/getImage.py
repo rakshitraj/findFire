c258c75b521fd83ed4f3815ab663231b51f2af5c
rakshitraj <rraj.rakshit@gmail.com>
2021-04-09 00:00:50 +0530

    modifications for Raspberry Pi local system

--- local/getImage.py
+++ local/getImage.py
@@ -1,34 +1,20 @@
-import cv2
 import time
 import os.path
+from picamera import PiCamera
 
 def recordData() :
-    key = cv2.waitKey(1)
-    camera = cv2.VideoCapture(0)
-    while True:
-        try:
-            check, frame = camera.read()
-            
-            ## Encoding to JPEG
-            # success, buffer = cv2.imencode(".jpg", frame)
-            
-            ## System Checks
-            # print(check)
-            # print(success)
-            # print(frame) # matrix value of images
-            # cv2.imshow("Capturing", frame)
-
-            # Save images
-            now = time.strftime('%d-%m-%Y %H%M%S')
-            filename = now+'.jpg'
-            path = '/home/raxit/findFire/local/raw'
-            cv2.imwrite(filename=os.path.join(path, filename), img=frame)
-            camera.release()
-            break
-        except:
-            # Turn off camera
-            camera.release()
-    return os.path.join(path, filename)
+    # Instantiate PiCamera object
+    camera = PiCamera()
+    # Generate Filename
+    now = time.strftime('%d-%m-%Y %H%M%S')
+    filename = now+'.jpg'
+    path = '/home/raxit/findFire/local/raw'
+    # Image path
+    image = os.path.join(path, filename)
+    # Capture Image
+    camera.capture(image)
+    # Return file-path
+    return image
 
 if __name__ == '__main__':
     filename = recordData()
